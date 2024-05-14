5ebb64e084325210520f09ae0ac66d1f0454fe21
Sachin Singh Dighan <sachinsinghd11@gmail.com>
2024-05-14 13:30:00 +0530

    Project setup

---
+++ app/src/main/java/com/sachin_singh_dighan/search_project/SearchActivity.kt
@@ -0,0 +1,11 @@
+package com.sachin_singh_dighan.search_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
